d2a1b55fe8e3b12b90d6218e25f0bc9d1aeab8e4
Craig Williams <20600064+MathWiz86@users.noreply.github.com>
2021-03-18 13:12:38 -0700

    Test Private Commit

---
+++ Source/MathOpLibrary/MathOpLibraryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MathOpLibraryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MATHOPLIBRARY_API AMathOpLibraryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
